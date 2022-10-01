095858aef2ea4d5ba41e19502375d0b6a563fd99
Pablo Dettori <106544349+p4bloxx@users.noreply.github.com>
2022-10-01 18:01:42 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const nav = document.querySelector(".primary-navigation");
 const toggleNav = document.querySelector(".mobile-nav-toggle");
 
-toggleNav.addEventListener("click", () => {
-    const visibility = nav.getAttribute("data-visible");
 
-    if (visibility === "false") {
-        nav.setAttribute("data-visible", true);
-        toggleNav.setAttribute("aria-expanded", true);
-    } else {
-        nav.setAttribute("data-visible", false);
-        toggleNav.setAttribute("aria-expanded", false);
-    }
-})
+toggleNav.addEventListener("click", (e) => {
+    const visibility = nav.getAttribute("data-visible");
+    const targ = e.target;
+
+        if (visibility === "false") {
+            nav.setAttribute("data-visible", true);
+            toggleNav.setAttribute("aria-expanded", true);
+        } else {
+            nav.setAttribute("data-visible", false);
+            toggleNav.setAttribute("aria-expanded", false);
+        }
+    
+        if (visibility === 'true' && targ !== toggleNav && targ !== nav) {
+            nav.setAttribute("data-visible", false);
+            toggleNav.setAttribute("aria-expanded", false);
+        }
+});
+
